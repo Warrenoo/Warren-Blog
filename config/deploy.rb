8601f4eb7c08003c6aa006c090c7f1182e6ba2a8
沈汪洋 <541991a@gmail.com>
2015-05-14 19:36:23 +0800

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,7 +13,7 @@ set :deploy_to, "/project/warren_blog"
 
 set :deploy_via, :remote_cache
 
-set :linked_files, %w{config/database.yml config/secrets.yml config/newrelic.yml Gemfile}
+set :linked_files, %w{config/database.yml config/secrets.yml config/newrelic.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/images}
 
 set :keep_releases, 5
