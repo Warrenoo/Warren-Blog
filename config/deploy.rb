303ac145d088bb5b5dbb5b750bcb13f6091d7928
沈汪洋 <541991a@gmail.com>
2015-05-14 12:50:06 +0800

    capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,67 +13,67 @@ set :deploy_to, "/project/warren_blog"
 
 set :deploy_via, :remote_cache
 
-set :linked_files, %w{config/database.yml config/secrets.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml config/newrelic.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 set :keep_releases, 5
 
-namespace :deploy do
+#namespace :deploy do
 
-  after :finishing, 'deploy:cleanup'
+  #after :finishing, 'deploy:cleanup'
 
-  task :upload do
-    on release_roles :app do |host|
-      upload! 'config/publisher.yml.example', "#{fetch(:deploy_to)}/shared/config/publisher.yml"
-    end
-  end
+  #task :upload do
+    #on release_roles :app do |host|
+      #upload! 'config/publisher.yml.example', "#{fetch(:deploy_to)}/shared/config/publisher.yml"
+    #end
+  #end
 
   
   
-  # 重启服务
-  task :start do
-    on roles(fetch(:unicorn_roles)) do
-      within current_path do
-        execute "cd #{current_path} && bundle exec bluepill --no-privileged start unicorn RAILS_ENV=#{fetch(:rails_env)}"
-      end
-    end
-  end
+  ## 重启服务
+  #task :start do
+    #on roles(fetch(:unicorn_roles)) do
+      #within current_path do
+        #execute "cd #{current_path} && bundle exec bluepill --no-privileged start unicorn RAILS_ENV=#{fetch(:rails_env)}"
+      #end
+    #end
+  #end
 
-  task :stop do
-    on roles(fetch(:unicorn_roles)) do
-      within current_path do
-        execute "cd #{current_path} && bundle exec bluepill --no-privileged stop unicorn RAILS_ENV=#{fetch(:rails_env)}"
-      end
-    end
-  end
-  task :restart do
-    on roles(fetch(:unicorn_roles)) do
-      if fetch(:rails_env) == 'production'
-        execute "cd #{current_path} && bundle exec bluepill --no-privileged restart unicorn RAILS_ENV=#{fetch(:rails_env)}"
-      else
-        invoke 'unicorn:restart'
-      end
-    end
-  end
+  #task :stop do
+    #on roles(fetch(:unicorn_roles)) do
+      #within current_path do
+        #execute "cd #{current_path} && bundle exec bluepill --no-privileged stop unicorn RAILS_ENV=#{fetch(:rails_env)}"
+      #end
+    #end
+  #end
+  #task :restart do
+    #on roles(fetch(:unicorn_roles)) do
+      #if fetch(:rails_env) == 'production'
+        #execute "cd #{current_path} && bundle exec bluepill --no-privileged restart unicorn RAILS_ENV=#{fetch(:rails_env)}"
+      #else
+        #invoke 'unicorn:restart'
+      #end
+    #end
+  #end
   
-  task :restart_unicorn do
-    on roles(fetch(:unicorn_roles)) do
-      if fetch(:rails_env) == 'production'
-        execute "cd #{current_path} && bundle exec bluepill --no-privileged restart unicorn RAILS_ENV=#{fetch(:rails_env)}"
-      else
-        invoke 'unicorn:restart'
-      end
-    end
-  end
+  #task :restart_unicorn do
+    #on roles(fetch(:unicorn_roles)) do
+      #if fetch(:rails_env) == 'production'
+        #execute "cd #{current_path} && bundle exec bluepill --no-privileged restart unicorn RAILS_ENV=#{fetch(:rails_env)}"
+      #else
+        #invoke 'unicorn:restart'
+      #end
+    #end
+  #end
 
-  task :stop_unicorn do
-    on roles(fetch(:unicorn_roles)) do
-      if fetch(:rails_env) == 'production'
-        execute "cd #{current_path} && bundle exec bluepill --no-privileged restart unicorn RAILS_ENV=#{fetch(:rails_env)}"
-      else
-        invoke 'unicorn:stop'
-      end
-    end
-  end
+  #task :stop_unicorn do
+    #on roles(fetch(:unicorn_roles)) do
+      #if fetch(:rails_env) == 'production'
+        #execute "cd #{current_path} && bundle exec bluepill --no-privileged restart unicorn RAILS_ENV=#{fetch(:rails_env)}"
+      #else
+        #invoke 'unicorn:stop'
+      #end
+    #end
+  #end
 
-end
+#end
